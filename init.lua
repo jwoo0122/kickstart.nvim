43d50fbae62bfb6ed688ce0cff3295a0014ba2d0
Jinwoo Jeong <leo@toss.im>
2024-06-19 00:28:41 +0900

    fix

--- init.lua
+++ init.lua
@@ -91,6 +91,8 @@ vim.cmd 'language en_US'
 vim.g.mapleader = '\\'
 vim.g.maplocalleader = '\\'
 
+vim.cmd 'set guicursor=a:hor10'
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
@@ -279,28 +281,28 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  -- { -- Useful plugin to show you pending keybinds.
-  --   'folke/which-key.nvim',
-  --   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  --   config = function() -- This is the function that runs, AFTER loading
-  --     require('which-key').setup()
-  --
-  --     -- Document existing key chains
-  --     require('which-key').register {
-  --       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  --       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  --       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  --       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  --       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-  --       ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  --       ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  --     }
-  --     -- visual mode
-  --     require('which-key').register({
-  --       ['<leader>h'] = { 'Git [H]unk' },
-  --     }, { mode = 'v' })
-  --   end,
-  -- },
+  { -- Useful plugin to show you pending keybinds.
+    'folke/which-key.nvim',
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    config = function() -- This is the function that runs, AFTER loading
+      require('which-key').setup()
+
+      -- Document existing key chains
+      require('which-key').register {
+        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      }
+      -- visual mode
+      require('which-key').register({
+        ['<leader>h'] = { 'Git [H]unk' },
+      }, { mode = 'v' })
+    end,
+  },
 
   -- NOTE: Plugins can specify dependencies.
   --
@@ -701,7 +703,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
       },
     },
   },
@@ -907,6 +909,9 @@ require('lazy').setup({
   {
     'stevearc/oil.nvim',
     opts = {
+      keymaps = {
+        ['s'] = '<Plug>(leap-forward)',
+      },
       view_options = {
         show_hidden = true,
       },
